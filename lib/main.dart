82dabfa72c680480af63fe4b1f9db80c6602945d
Muhammad Alwan Naufal <alwnufal@gmail.com>
2021-08-03 15:26:24 +0700

    assets, splash screen, get started.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/get-started': (context) => GetStartedPage(),
+      },
+    );
+  }
+}
